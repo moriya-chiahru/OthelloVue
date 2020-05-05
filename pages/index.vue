71489516eecd973739ab8cf76705668d32f4d147
moriya <mry116k@hotmail.com>
2020-05-05 19:50:25 +0900

    裏返す処理追加

--- pages/index.vue
+++ pages/index.vue
@@ -10,10 +10,9 @@
         <div
           v-if="hasStone(x - 1, y - 1)"
           :class="['stone', isBlack(x - 1, y - 1) ? 'black' : 'white']"
-        >
-          x:{{ x - 1 }}y:{{ y - 1 }}
-        </div>
-        <div v-else>x:{{ x - 1 }}y:{{ y - 1 }}</div>
+        />
+        <!-- x:{{ x - 1 }}y:{{ y - 1 }} -->
+        <!-- <div v-else>x:{{ x - 1 }}y:{{ y - 1 }}</div> -->
       </div>
     </template>
     <p>{{ currentStone }}のターン</p>
@@ -29,7 +28,21 @@ border: ボード、マス情報
 y: マスの縦位置
 x: マスの横位置 */
 type Board = Array<Array<number>>
-type Cell = { x: number; y: number; color: number }
+type Cell = {
+  x: number
+  y: number
+  color?: number
+  direction?:
+    | 'center'
+    | 'north'
+    | 'northeast'
+    | 'east'
+    | 'southeast'
+    | 'south'
+    | 'southwest'
+    | 'west'
+    | 'northwest'
+}
 enum CellTypeEnum {
   WHITE = 'WHITE',
   BLACK = 'BLACK',
@@ -47,8 +60,8 @@ export default class extends Vue {
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 1, 2, 2, 2, 2],
-    [0, 0, 0, 2, 1, 1, 1, 1],
+    [0, 0, 0, 1, 2, 0, 0, 0],
+    [0, 0, 0, 2, 1, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0]
@@ -57,6 +70,17 @@ export default class extends Vue {
   get currentStone(): CellTypeEnum {
     return this.currentColor === 1 ? CellTypeEnum.WHITE : CellTypeEnum.BLACK
   }
+
+  directionList: Cell[] = [
+    { x: 0, y: -1, direction: 'north' },
+    { x: 1, y: -1, direction: 'northeast' },
+    { x: 1, y: 0, direction: 'east' },
+    { x: 1, y: 1, direction: 'southeast' },
+    { x: 0, y: 1, direction: 'south' },
+    { x: -1, y: 1, direction: 'southwest' },
+    { x: -1, y: 0, direction: 'west' },
+    { x: -1, y: -1, direction: 'northwest' }
+  ]
 
   currentColor: number = 1
 
@@ -78,58 +102,114 @@ export default class extends Vue {
 
   /* 選択した周りと自身のセルを取得 */
   getSelectedAroundCells = (selectX: number, selectY: number): Cell[] => {
-    const row = [selectX - 1, selectX, selectX + 1]
-    const collum = [selectY - 1, selectY, selectY + 1]
-    /* TODO:処理を外に出す */
-    if (selectX === 0) {
-      row.shift()
-    } else if (selectX + 1 === this.board.length) {
-      row.pop()
-    }
-    if (selectY === 0) {
-      collum.shift()
-    } else if (selectY + 1 === this.board.length) {
-      collum.pop()
-    }
-    return row
-      .map((x: number): Cell[] => {
-        return collum.map(
-          (y: number): Cell => {
-            return { x, y, color: this.board[y][x] }
-          }
-        )
-      })
-      .flat()
-      .filter(
-        (cell: Cell): boolean => !(cell.x === selectX && cell.y === selectY)
-      )
+    return this.directionList.map(
+      (direction: Cell): Cell => {
+        const x: number = selectX + direction.x
+        const y: number = selectY + direction.y
+        return {
+          x,
+          y,
+          color:
+            this.getSelectedConner(x) && this.getSelectedConner(y)
+              ? this.board[y][x]
+              : 0,
+          direction: direction.direction
+        }
+      }
+    )
   }
 
-  /* 選択したセルが端か判定する
-  getSelectedAroundCells = (selectIndex: number, pointers: number[]): number[] => {
-  } */
-
-  canPut = (selectX: number, selectY: number, selectColor: number): boolean => {
-    const aroundOpponentCellsInfo: Cell[] = this.getAroundOpponentCellsInfo(
-      selectX,
-      selectY,
-      selectColor
-    )
-    const selectCell: Cell = {
-      x: selectX,
-      y: selectY,
-      color: this.board[selectY][selectX]
+  /* 選択したセルが端か判定する */
+  getSelectedConner = (position: number): boolean => {
+    if (position === -1 || position === 8) {
+      return false
     }
+    return true
+  }
+
+  /* 裏返すセルを返す */
+  getReturnCells = (
+    aroundOpponentCellsInfos: Cell[],
+    selectColor: number
+  ): Cell[] => {
+    return aroundOpponentCellsInfos
+      .map((aroundOpponentCellsInfo: Cell): Cell[] => {
+        const returnCells: Cell[] = [aroundOpponentCellsInfo]
+        // ループする方向を得る
+        const direction: Cell | undefined = this.directionList.find(
+          (directionList: Cell): boolean =>
+            directionList.direction === aroundOpponentCellsInfo.direction
+        )
+
+        if (direction) {
+          let searchX: number = aroundOpponentCellsInfo.x + direction.x
+          let searchY: number = aroundOpponentCellsInfo.y + direction.y
+          // 端までループし状態を確認
+          while (
+            this.getSelectedConner(searchX) &&
+            this.getSelectedConner(searchY) &&
+            this.board[searchY][searchX] !== CellTypeNumberEnum.NONE
+          ) {
+            const getCellColor: number = this.board[searchY][searchX]
+            returnCells.push({
+              x: searchX,
+              y: searchY,
+              color: getCellColor,
+              direction: direction.direction
+            })
+            // 同じ色のセルまで行き着いたら抜ける
+            if (getCellColor === selectColor) {
+              break
+            }
+            // 位置情報を更新
+            searchX += direction.x
+            searchY += direction.y
+          }
+        }
+        // 最後のセルが同じ色ではなかったら、空を返す
+        const endReturnCell: Cell | undefined = returnCells.pop()
+        return endReturnCell && endReturnCell.color === selectColor
+          ? returnCells
+          : []
+      })
+      .flat()
+  }
+
+  canPut = (
+    selectX: number,
+    selectY: number,
+    aroundOpponentCellsInfos: Cell[]
+  ): boolean => {
     return (
-      aroundOpponentCellsInfo.length > 0 &&
-      selectCell.color === CellTypeNumberEnum.NONE
+      aroundOpponentCellsInfos.length > 0 &&
+      this.board[selectY][selectX] === CellTypeNumberEnum.NONE
     )
   }
 
   onClick(x: number, y: number) {
-    if (this.canPut(x, y, this.currentColor)) {
-      this.$set(this.board[y], x, this.currentColor)
+    const aroundOpponentCellsInfos: Cell[] = this.getAroundOpponentCellsInfo(
+      x,
+      y,
+      this.currentColor
+    )
+    const returnCells: Cell[] = this.getReturnCells(
+      aroundOpponentCellsInfos,
+      this.currentColor
+    )
+
+    if (this.canPut(x, y, returnCells)) {
+      // 現在の位置を追加
+      returnCells.unshift({
+        x,
+        y,
+        color: this.currentColor,
+        direction: 'center'
+      })
+      // セルを更新
       this.currentColor = 3 - this.currentColor
+      returnCells.forEach((returnCell: Cell) => {
+        this.$set(this.board[returnCell.y], returnCell.x, 3 - this.currentColor)
+      })
     }
   }
 }
@@ -147,7 +227,6 @@ export default class extends Vue {
   float: left;
   width: 12.5%;
   height: 12.5%;
-  color: #f00;
   border: 1px solid #000;
 }
 
